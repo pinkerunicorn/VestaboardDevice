c03aab24c2ddd4a468d8e2435bbba8483be42660
Florian Graßinger <grassinger@ubyte.gmbh>
2026-06-24 20:25:59 +0200

    Cleanup: Remove legacy Vestaboard event types from UI and logic

--- VestaboardGenerator/module.php
+++ VestaboardGenerator/module.php
@@ -123,31 +123,6 @@ class VestaboardGenerator extends IPSModule {
                     $text = $this->GenerateProgressBar($prefix, $prozent, $color);
                 }
                 break;
-            case 'brief':
-                if (GetValue($id)) {
-                    $text = $this->PadToRight("Briefkasten voll", "{65}"); // {65} Gelb
-                }
-                break;
-            case 'muell':
-                $muell = GetValue($id);
-                if ($muell == 1) $text = $this->PadToRight("Biotonne", "{66}"); // Grün
-                if ($muell == 2) $text = $this->PadToRight("Papiertonne", "{67}"); // Blau
-                if ($muell == 3) $text = $this->PadToRight("Restmuell", "{70}"); // Schwarz
-                break;
-            case 'sbahn':
-                $sbahnWert = (string)GetValue($id);
-                // Wenn Format hinterlegt, nimm das, sonst Default
-                if ($format != "") {
-                    if (strpos($format, '%s') !== false) {
-                        $text = sprintf($format, $sbahnWert);
-                    } else {
-                        $text = $format . $sbahnWert;
-                    }
-                } else {
-                    $text = "S2: " . $sbahnWert;
-                }
-                $text = $this->PadToRight($text, ""); 
-                break;
             case 'aussen':
                 $temp = (float)GetValue($id);
                 $color = "{69}"; // Weiß (Neutral)
@@ -167,44 +142,6 @@ class VestaboardGenerator extends IPSModule {
                 }
                 
                 $text = $this->PadToRight($textStr, $color);
-                break;
-            case 'garten':
-                $val = GetValue($id);
-                $isActive = false;
-                
-                if (is_bool($val)) {
-                    $isActive = $val;
-                } else if (is_int($val) || is_float($val)) {
-                    $isActive = ($val > 0);
-                } else if (is_string($val)) {
-                    // Kompatibilität mit SmartLawnAI "SummaryStatus" oder "Status_X"
-                    $valUpper = strtoupper($val);
-                    if (strpos($valUpper, 'WATERING') !== false || strpos($valUpper, 'BEWÄSSERE') !== false || strpos($valUpper, 'BEWAESSERE') !== false) {
-                        $isActive = true;
-                    }
-                }
-
-                if ($isActive) {
-                    // Blaue Anzeige {67} für aktives Wasser
-                    $text = $this->PadToRight("Gartenbewaesserung", "{67}"); 
-                }
-                break;
-            case 'fenster':
-                $val = (int)GetValue($id);
-                if ($val > 0) {
-                    // Rote Anzeige {63} für offene Sicherheitsbereiche
-                    $text = $this->PadToRight("Offen: " . $val, "{63}"); 
-                }
-                break;
-            case 'keller':
-                if (GetValue($id)) {
-                    $text = $this->PadToRight("Keller lueften", "{67}"); // {67} Blau
-                }
-                break;
-            case 'ofen':
-                if (GetValue($id)) {
-                    $text = $this->PadToRight("Ofen: Angefeuert", "{64}"); // {64} Orange
-                }
                 break;
             case 'custom':
                 $val = GetValue($id);
